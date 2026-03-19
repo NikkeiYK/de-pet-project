178aa345d3c6dae34ec119fee394c7d210f41c70
Tekai_ <vip007_007@mail.ru>
2026-03-19 16:19:36 +0300

    обновил логику dag

--- dags/extract_from_api_to_tmp.py
+++ dags/extract_from_api_to_tmp.py
@@ -5,17 +5,21 @@ import requests
 import pandas as pd
 import os
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.utils.task_group import TaskGroup
+from airflow.decorators import task
+from airflow.models import Variable
+from airflow.hooks.base import BaseHook
 
-URL_API = "https://jsonplaceholder.typicode.com/posts"
-DATA_DIR = "/tmp/files"
+
+DATA_DIR = Variable.get("DATA_DIR")
 CHUNK_SIZE = 60
 DAG_ID = "extract_from_api_to_tmp"
+conn = BaseHook.get_connection("api_jsonplaceholder")
+BASE_URL = f"{conn.host}/posts"
 
-os.makedirs(DATA_DIR, exist_ok=True)
 
-
+@task
 def get_data(url: str):
     try:
         res = requests.get(url)
@@ -27,6 +31,7 @@ def get_data(url: str):
         return []
 
 
+@task
 def chunking_data(data, output_dir: str, chunk_size: int):
     if not data:
         print("Поступил пустой массив данных")
@@ -35,25 +40,21 @@ def chunking_data(data, output_dir: str, chunk_size: int):
     total_chunks = (len(data) + chunk_size - 1) // chunk_size
     print(f"Разбиваем данные длиной {len(data)} на {total_chunks} чанков.")
 
-    for i in range(0, len(data), chunk_size):
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    filenames = []
+
+    for idx, i in enumerate(range(0, len(data), chunk_size)):
         chunk = data[i:i + chunk_size]
-        filename = f"{output_dir}/chunk_{1 if i == 0 else i // chunk_size}.txt"
+        filename = f"{output_dir}/chunk_{idx + 1}.txt"
         with open(filename, 'w', encoding="utf-8") as file:
             for el in chunk:
                 file.write(json.dumps(el) + '\n')
+            filenames.append(filename)
+    return filenames
 
 
-def extract_pipeline(**context):
-    data = get_data(URL_API)
-
-    chunking_data(data, DATA_DIR, CHUNK_SIZE)
-
-
-def get_txt_files_list(**context):  # ✅ Новая функция для динамического списка
-    return [os.path.join(DATA_DIR, f) for f in os.listdir(DATA_DIR)
-            if f.endswith('.txt')]
-
-
+@task(sla=timedelta(minutes=2))
 def analyze_txt_chunk(filename: str):
     """Анализирует один .txt файл - возвращает размер и строки"""
     size_bytes = os.path.getsize(filename)
@@ -73,37 +74,39 @@ def analyze_txt_chunk(filename: str):
     }
 
 
+@task
+def collect_result(data: list):
+    for i in data:
+        print(f"Filename: {i["filename"]}, size: {i["size_mb"]}, lines: {i["lines"]}")
+
+
 with DAG(
     dag_id=DAG_ID,
     schedule="@once",
     catchup=False,
-    start_date=datetime(2026, 3, 17)
+    start_date=datetime(2026, 3, 17),
+    tags=["example", "taskflow"]
 ) as dag:
-    start = EmptyOperator("Start")
+    dag.doc_md = __doc__
 
-    task_extract = PythonOperator(
-        task_id="extract_pipeline",
-        python_callable=extract_pipeline,
-    )
+    start = EmptyOperator(task_id="Start")
 
-    list_files_task = PythonOperator(
-        task_id="get_txt_files_list",
-        python_callable=get_txt_files_list
-    )
+    data = get_data(BASE_URL)
+
+    filenames = chunking_data(data, DATA_DIR, CHUNK_SIZE)
 
     with TaskGroup("analyze_chunks") as analyze_group:
 
         analyse_start = EmptyOperator(task_id="Analyse_start")
 
-        analyze_tasks = PythonOperator.partial(
-            task_id="analyze_txt_chunk",
-            python_callable=analyze_txt_chunk,
-        ).expand(
-            # Ключ 'filename' должен совпадать с аргументом в функции analyze_txt_chunk
-            # Значение - это ссылка на выход (output/XCom) предыдущей задачи
-            filename=list_files_task.output
-        )
+        result = analyze_txt_chunk.expand(filename=filenames)
+
+        collection = collect_result(result)
+
+        analyse_end = EmptyOperator(task_id="Analyse_end")
+
+        analyse_start >> result >> collection >> analyse_end
 
     end = EmptyOperator(task_id="End")
 
-    start >> task_extract >> list_files_task >> analyze_group >> end
+    start >> data >> filenames >> analyze_group >> end
